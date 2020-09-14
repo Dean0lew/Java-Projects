ee6d44ab2221eb64680f08e72e5ae6cd5e0e0f3e
Dean0lew <dean0lew@gmail.com>
2020-09-14 18:23:53 -0400

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Temperature
+{
+    public static void main(String[] args)
+    {
+        //Variable declaration
+        double temp = 0;
+
+        //Instantiating a Scanner object to get user input
+        Scanner sc = new Scanner(System.in);
+
+        //Get temperature from user
+        System.out.print( "Enter a temperature:  ");
+        temp = sc.nextInt();
+
+        //Display message based on temperature
+        if (temp <= 32)
+            System.out.println("It's freezing.");
+
+        if (temp < 50)
+            System.out.println("It's cold.");
+
+        if (temp < 80)
+            System.out.println("It's warm.");
+
+        if (temp < 100)
+        System.out.println("It's hot.");
+
+      else
+        System.out.println("It's extremely hot!");
+
+    }//end main
+}//end class
